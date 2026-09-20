f8cc78bfa418de1562ac863ff8d269c51c6c99c9
Mohan Raj <rajmohan4419@gmail.com>
2026-09-20 08:34:32 +0530

    Track visits and page views

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import ToolsHome from './pages/ToolsHome';
 import ToolPage from './pages/ToolPage';
+import { recordVisit, logEvent } from './lib/telemetry';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Telemetry />
       <Routes>
         <Route path="/" element={<ToolsHome />} />
         <Route path="/tools" element={<ToolsHome />} />
@@ -13,4 +16,17 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+function Telemetry() {
+  const location = useLocation();
+
+  useEffect(() => {
+    recordVisit(location.pathname).then(total => {
+      window.dispatchEvent(new CustomEvent('orbitboard:visitor-count', { detail: total }));
+    });
+    logEvent('page.viewed', { path: location.pathname });
+  }, [location.pathname]);
+
+  return null;
 }
