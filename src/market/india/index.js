4090bf7879b03436ff40524a9617ed956f055d18
Mohan Raj <rajmohan4419@gmail.com>
2026-09-26 12:26:32 +0530

    feat(market-lab): add Indian primary source connectors

---
+++ src/market/india/index.js
@@ -0,0 +1,10 @@
+export {
+  NSE_PRIMARY_SOURCE,
+  BSE_PRIMARY_SOURCE,
+  SEBI_PRIMARY_SOURCE
+} from './indiaSource';
+export {
+  INDIA_CONNECTOR_CAPABILITIES,
+  createIndiaFilingConnector,
+  runIndiaFilingConnector
+} from './indiaConnector';
