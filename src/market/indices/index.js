c803596aa0d9b51ec121891427caa2735422394d
Mohan Raj <rajmohan4419@gmail.com>
2026-09-26 18:03:48 +0530

    feat(market-lab): add first-class index research model

---
+++ src/market/indices/index.js
@@ -0,0 +1,3 @@
+export { INDEX_TYPES, MARKET_INDEXES, resolveIndex } from './indexModel';
+export { indexRecordsToEvidence } from './indexEvidenceAdapter';
+export { INDEX_CONNECTOR_CAPABILITIES, createIndiaIndexConnector, runIndiaIndexConnector } from './indexConnector';
