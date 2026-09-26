542c4015bae417e121cb5bd06dc9bce7f3753f59
Mohan Raj <rajmohan4419@gmail.com>
2026-09-26 11:51:57 +0530

    feat(market-lab): add source adapter contract

---
+++ src/market/sources/index.js
@@ -0,0 +1,10 @@
+export {
+  SOURCE_CAPABILITIES,
+  SOURCE_TRUST,
+  createSourceDescriptor,
+  assertSourceAdapter,
+  normalizeAdapterResult
+} from './sourceContract';
+
+export { normalizeSourcePayload } from './normalizer';
+export { demoSource, demoAdapter } from './demoAdapter';
